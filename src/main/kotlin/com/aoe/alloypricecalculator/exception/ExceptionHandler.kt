1d142599c6027ae8ff9a92a1de9bd86ea7fc2e86
manish.kharel <manish.kharel@aoe.com>
2023-01-13 15:28:41 +0100

    fix: exception handler class fixed

--- src/main/kotlin/com/aoe/alloypricecalculator/exception/ExceptionHandler.kt
+++ src/main/kotlin/com/aoe/alloypricecalculator/exception/ExceptionHandler.kt
@@ -26,12 +26,6 @@ class ExceptionHandler : ResponseEntityExceptionHandler() {
 
   @ExceptionHandler(value = [(MetalsApiFailedException::class)])
   fun handleMetalsApiFailedException(ex: MetalsApiFailedException): ResponseEntity<List<MetalPrice>> {
-
-    val headers = LinkedMultiValueMap<String, String>()
-    headers.add(HttpHeaders.WARNING, "NOT_REAL_TIME")
-
-    return ResponseEntity(ex.prices, headers, HttpStatus.OK)
-
     return ResponseEntity(ex.prices, HttpStatus.NON_AUTHORITATIVE_INFORMATION)
   }
 
